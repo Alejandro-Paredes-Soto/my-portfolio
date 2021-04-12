2eb3d35ca68278bf013e6fa68ea7d7d84d85d543
Alejandro-Paredes_Soto <alejandroparedessoto18@gmail.com>
2021-04-11 21:30:40 -0500

    my work of portfolio

---
+++ js.js
@@ -0,0 +1,9 @@
+const hamburguer = document.getElementById('hamburguer');
+const sub_menu = document.getElementById('sub-menu');
+
+hamburguer.addEventListener('click', ()=>{
+
+sub_menu.classList.toggle('show')
+
+
+})
